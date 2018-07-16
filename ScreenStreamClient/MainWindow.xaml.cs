3ab30aa4fb9b818db49e92b82cec183cf264d5fb
Хамелеон <maxali946@gmail.com>
2018-07-17 00:40:40 +0400

    Add project files.

---
+++ ScreenStreamClient/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ScreenStreamClient
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp); // 65Kb
+            var ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 7534);
+
+            socket.Bind(ep);
+
+            var bytes = new byte[socket.ReceiveBufferSize];
+            EndPoint client = new IPEndPoint(IPAddress.Any, 0);
+
+            while (true)
+            {
+                var length = socket.ReceiveFrom(bytes, ref client);
+                var msg = Encoding.Default.GetString(bytes, 0, length);
+            }
+        }
+    }
+}
---
+++ ScreenStreamServer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenStreamServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            var ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 7534);
+
+            var answer = new byte[8192];
+
+            while (true)
+            {
+                var msg = Console.ReadLine(); ;
+                var data = Encoding.Default.GetBytes(msg);
+                socket.SendTo(data, ep);
+            }
+        }
+    }
+}
