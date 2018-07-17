fa21e52233d211ca5f8caffe9aa54a20dd6dd464
Хамелеон <maxali946@gmail.com>
2018-07-17 12:02:59 +0400

    70%

--- ScreenStreamClient/MainWindow.xaml.cs
+++ ScreenStreamClient/MainWindow.xaml.cs
@@ -1,41 +1,194 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.ComponentModel;
 using System.Net;
 using System.Net.Sockets;
+using System.Runtime.CompilerServices;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ScreenStreamClient
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        Socket socket;
+        EndPoint ep;
+
+        private string conDisConIp;
+        public string ConDisConIp
+        {
+            get { return conDisConIp; }
+            set { conDisConIp = value; OnChanged(); }
+        }
+
+        private string screenPic;
+        public string ScreenPic
+        {
+            get { return screenPic; }
+            set { screenPic = value; OnChanged(); }
+        }
+
+        private Visibility conButVis;
+        public Visibility ConButVis
+        {
+            get { return conButVis; }
+            set { conButVis = value; OnChanged(); }
+        }
+
+        private Visibility disconButVis = Visibility.Collapsed;
+        public Visibility DisconButVis
+        {
+            get { return disconButVis; }
+            set { disconButVis = value; OnChanged(); }
+        }
+
+        private Visibility streamingPanelVis;
+        public Visibility StreamingPanelVis
+        {
+            get { return streamingPanelVis; }
+            set { streamingPanelVis = value; OnChanged(); }
+        }
+
+        private Visibility pauseButVis;
+        public Visibility PauseButVis
+        {
+            get { return pauseButVis; }
+            set { pauseButVis = value; OnChanged(); }
+        }
+
+        private Visibility playButVis;
+        public Visibility PlayButVis
+        {
+            get { return playButVis; }
+            set { playButVis = value; OnChanged(); }
+        }
+
+        //----------------------------------------------------------------------
+
+        private ICommand connectCom;
+        public ICommand ConnectCom
+        {
+            get
+            {
+                if (connectCom is null)
+                {
+                    connectCom = new RelayCommand(
+                        (param) =>
+                        {
+                            socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                            ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 7534);
+
+                            Streaming();
+                        },
+                        (param) =>
+                        {
+                            if (ConDisConIp != "")
+                                return true;
+                            else
+                                return false;
+                        });
+                }
+
+                return connectCom;
+            }
+        }
+
+        private ICommand disconnectCom;
+        public ICommand DisconnectCom
+        {
+            get
+            {
+                if (disconnectCom is null)
+                {
+                    disconnectCom = new RelayCommand(
+                        (param) =>
+                        {
+                            socket.Shutdown(SocketShutdown.Both);
+                            socket.Close();
+
+                            ConButVis = Visibility.Visible;
+                            DisconButVis = Visibility.Collapsed;
+                        });
+                }
+
+                return disconnectCom;
+            }
+        }
+
+        private ICommand pauseCom;
+        public ICommand PauseCom
+        {
+            get
+            {
+                if (pauseCom is null)
+                {
+                    pauseCom = new RelayCommand(
+                        (param) =>
+                        {
+
+                        });
+                }
+
+                return pauseCom;
+            }
+        }
+
+        private ICommand playCom;
+        public ICommand PlayCom
+        {
+            get
+            {
+                if (playCom is null)
+                {
+                    playCom = new RelayCommand(
+                        (param) =>
+                        {
+
+                        });
+                }
+
+                return playCom;
+            }
+        }
+
+        //----------------------------------------------------------------------
+
         public MainWindow()
         {
             InitializeComponent();
 
-            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp); // 65Kb
-            var ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 7534);
+            DataContext = this;
+        }
 
-            socket.Bind(ep);
+        //----------------------------------------------------------------------
 
-            var bytes = new byte[socket.ReceiveBufferSize];
-            EndPoint client = new IPEndPoint(IPAddress.Any, 0);
+        void Streaming()
+        {
+            var answer = new byte[8192];
 
             while (true)
             {
-                var length = socket.ReceiveFrom(bytes, ref client);
-                var msg = Encoding.Default.GetString(bytes, 0, length);
+                var msg = "Connect";
+                var data = Encoding.Default.GetBytes(msg);
+                socket.SendTo(data, ep);
+
+                var length = socket.Receive(answer);
+
+                if (length != 0)
+                {
+                    MessageBox.Show(answer.ToString());
+                }
             }
         }
+
+        //----------------------------------------------------------------------
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnChanged([CallerMemberName]string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        //----------------------------------------------------------------------
     }
 }
--- ScreenStreamServer/Program.cs
+++ ScreenStreamServer/Program.cs
@@ -1,28 +1,83 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+﻿using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
+using System.Timers;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Drawing.Imaging;
+using System.IO.Compression;
 
 namespace ScreenStreamServer
 {
     class Program
     {
+        //static bool PlayStop = true;
+        //static System.Timers.Timer timer;
+
         static void Main(string[] args)
         {
-            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            //timer = new System.Timers.Timer();
+            //timer.Interval = 10;
+            
+            //timer.Elapsed += SendScreen;
+            //timer.AutoReset = true;
+            //timer.Enabled = true;
+
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp); // 65Kb
             var ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 7534);
 
-            var answer = new byte[8192];
+            socket.Bind(ep);
+
+            var bytes = new byte[socket.ReceiveBufferSize];
 
             while (true)
             {
-                var msg = Console.ReadLine(); ;
-                var data = Encoding.Default.GetBytes(msg);
-                socket.SendTo(data, ep);
+                EndPoint client = new IPEndPoint(IPAddress.Any, 0);
+
+                while (true)
+                {
+                    var length = socket.ReceiveFrom(bytes, ref client);
+                    var msg = Encoding.Default.GetString(bytes, 0, length);
+
+                    if (msg != "")
+                    {
+                        var sendBytes = SendScreen();
+                        socket.SendTo(sendBytes, client);
+
+                        msg = string.Empty;
+                    }
+                    else
+                        break;
+                } 
             }
+        }
+
+        //------------------------------------------------------------------------
+
+        static byte[] SendScreen()
+        {
+            Graphics graph = null;
+            var bmp = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            graph = Graphics.FromImage(bmp);
+            graph.CopyFromScreen(0, 0, 0, 0, bmp.Size);
+
+            using (var mStream = new MemoryStream())
+            {
+                bmp.Save(mStream, ImageFormat.Jpeg);
+
+            }
+            return mStream.ToArray();
+
+            using (var wms = new MemoryStream())
+            {
+                using (var ds = new DeflateStream(wms, CompressionMode.Compress))
+                {
+                    ds.Write();
+                } 
+            }
+
         }
     }
 }
